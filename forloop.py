ddc617ed92c72b59e483af6e58edc2455311a2aa
Sourav7526 <Sk5164203@gmail.com>
2026-02-02 12:43:37 +0530

    update

--- forloop.py
+++ forloop.py
@@ -37,4 +37,15 @@
 #     print("Palindrome")
 # else:
 #     print("Not Palindrome")
-#jdjdj
+
+
+#5 question 
+#for i in range(1,51):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("fizzbuzz")
+#     elif i % 3 == 0:
+#         print("fizz")
+#     elif i % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(i)
